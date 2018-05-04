83a3286634ae072b285cc54d198dbe778faab77e
ageitmann <geitmann@simutron.de>
2018-05-04 11:43:14 +0200

    Initial commit

---
+++ includes/class-plugin-name-i18n.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ * @author     Your Name <email@example.com>
+ */
+class Plugin_Name_i18n {
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain(
+			'plugin-name',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+	}
+}
